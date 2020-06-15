112fd9825c917300b55423614b04d38315ed0323
Elliott <elliott@grouloo.fr>
2020-06-15 17:54:06 +0200

    Add layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: ['prop-types', 'react', 'semantic-ui-react'],
 }
